57bbcd35f134b0e3c87267e0aec6616c5be9bce2
Carl Luberti <cluberti@cluberti.com>
2024-10-31 16:18:32 -0700

    Add files via upload
    Updated to handle Server 2016/2019/2022/2025 ISO downloads.

--- Download-WindowsESD.ps1
+++ Download-WindowsESD.ps1
@@ -6,19 +6,19 @@ Param(
         Mandatory=$False
         )]
         [ValidateNotNull()]
-        [string]$Path = "C:\Temp\ISODownloader",
+        [string]$Path = "C:\Temp\ESDDownloader",
     [Parameter(
         Position=2,
         Mandatory=$False
         )]
-        [ValidateSet('Win10', 'Win11')]
+        [ValidateSet('Win10', 'Win11', 'Server2016', 'Server2019', 'Server2022', 'Server2025')]
         [string]$OSVer = "Win11",
     [Parameter(
         Position=3,
         Mandatory=$False
         )]
         [ValidateSet('x64', 'ARM64', 'AMD64', 'ARM')]
-        [string]$Arch = "ARM64",
+        [string]$Arch = "x64",
     [Parameter(
         Position=4,
         Mandatory=$False
@@ -34,13 +34,14 @@ Param(
 # Get script start time (will be used to determine how long execution takes)
 $Script_Start_Time = (Get-Date).ToShortDateString()+", "+(Get-Date).ToLongTimeString()
 
-# Warn the user - this script was written in 30 minutes on a whim, so only the most basic error handling is here
+# Warn the user - this script was cobbled together from functions in other unrelated scripts in ~30 minutes on a whim and edited once just to add Server ISO support, so only the most basic error handling is here
 Clear-Host
-Write-Host "This script *requires* the Windows 10/11 ADK and 7Zip to be installed (due to issues with products.cab downloaded from Microsoft)." -ForegroundColor Yellow
-Write-Host "This script also only works on ESD files that contain images that can be parsed as 'Pro' or 'Enterprise'." -ForegroundColor Yellow
-Write-Host "Please feel free to fork or fix if you want to change that." -ForegroundColor Green
+Write-Host "If downloading a Windows client ESD, this script *requires* the Windows 10/11 ADK and 7Zip to be installed (due to issues with products.cab downloaded from Microsoft)." -ForegroundColor Yellow
+Write-Host "This script also only works on ESD files that contain images that can be parsed as 'Pro' or 'Enterprise' in this scenario." -ForegroundColor Yellow
+Write-Host "Please feel free to fork or submit a fix if you want to change that." -ForegroundColor Gray
 Write-Host ""
 Write-Host "Please press BREAK/CTRL+C to exit within the next 10 seconds if you do not meet both of the pre-requisites or this script will fail." -ForegroundColor Cyan
+Write-Host ""
 Start-Sleep 10
 
 # Normalize architecture strings
@@ -51,6 +52,38 @@ If ($Arch -eq 'AMD64')
 ElseIf ($Arch -eq 'ARM')
 {
     $Arch = 'ARM64'
+}
+
+# If Server, validate languages/architecture actually available are chosen
+If ($OSVer -like "Server*")
+{
+    $ServerLang = @(
+        "en-us",
+        "zh-cn",
+        "fr-fr",
+        "de-de",
+        "it-it",
+        "ja-jp",
+        "ru-ru",
+        "es-es"
+    )
+
+    If ($Arch -eq 'ARM64')
+    {
+        Write-Host "ARM64 Server ISOs are not currently available for download.  Please retry with 'x64' or 'AMD64' as the architecture, exiting script." -ForegroundColor Yellow
+        EXIT
+    }
+
+    If ($ServerLang -notcontains $Lang)
+    {
+        Write-Host "You have chosen to download $($OSVer) but have not chosen a valid language code.  Please retry with a valid language code, exiting script." -ForegroundColor Yellow
+        Write-Host "Valid language codes:" -ForegroundColor Magenta
+        ForEach ($Language in $ServerLang)
+        {
+            Write-Host $Language -ForegroundColor Green
+        }
+        EXIT
+    }
 }
 
 
@@ -200,7 +233,7 @@ Function Extract-File
     Check-PathExists -Path $Path -DeleteIfExists $True
     $FileExtension = [System.IO.Path]::GetExtension("$File")
     Write-Log -Message "Extracting file $($File) to $($Path)..."
-    Write-Host ""
+    Write-Log  " "
     If ($FileExtension -eq '.msi')
     {
         Try
@@ -230,7 +263,7 @@ Function Extract-File
         {
             Start-Process -FilePath $Exe -ArgumentList $argumentList -Wait -WindowStyle Hidden
             Write-Log -Message "Completed extraction to $Path."
-            Write-Host ""
+            Write-Log " "
         }
         Catch
         {
@@ -244,7 +277,7 @@ Function Extract-File
         {
             Expand-Archive -Path $File -DestinationPath $Path
             Write-Log -Message "Completed extraction to $Path."
-            Write-Host ""
+            Write-Log " "
         }
         Catch
         {
@@ -313,15 +346,17 @@ Function Download-File
     }
     Else
     {
-        Write-Log -Message "File $($Filename) already exists, skipping download"
-        Write-Host ""
+        Write-Log -Message "File $($FileToDownload) already exists, skipping download"
+        Write-Log " "
     }
+
+
     Return $FileToDownload
 }
 
 
-# Find the latest Windows ESD file and download it
-Function Find-WindowsESD
+# Find the latest Windows ESD or ISO file and download it
+Function Find-WindowsESDorISO
 {
     Param (
         $OSVer,
@@ -330,6 +365,20 @@ Function Find-WindowsESD
         $Path
     )
 
+    # Server URL language codes for adding to fwlink to get the right Server ISO
+    Switch ($Lang)
+    {
+        "en-us" {$LangCode = "&clcid=0x409&culture=en-us&country=US"}
+        "zh-cn" {$LangCode = "&clcid=0x804&culture=zh-cn&country=CN"}
+        "fr-fr" {$LangCode = "&clcid=0x40c&culture=fr-fr&country=FR"}
+        "de-de" {$LangCode = "&clcid=0x407&culture=de-de&country=DE"}
+        "it-it" {$LangCode = "&clcid=0x410&culture=it-it&country=IT"}
+        "ja-jp" {$LangCode = "&clcid=0x411&culture=ja-jp&country=JP"}
+        "ru-ru" {$LangCode = "&clcid=0x419&culture=ru-ru&country=RU"}
+        "es-es" {$LangCode = "&clcid=0x40a&culture=es-es&country=ES"}
+    }
+
+    # Set the correct URL and Path
     If ($OSVer -eq 'Win10')
     {
         $URL = "https://go.microsoft.com/fwlink/?LinkId=841361"
@@ -340,36 +389,74 @@ Function Find-WindowsESD
         $URL = "https://go.microsoft.com/fwlink/?LinkId=2156292"
         $Path = $Path + "\Win11"
     }
+    ElseIf ($OSVer -eq 'Server2016')
+    {
+        $FWLink = "https://go.microsoft.com/fwlink/p/?LinkID=2195174"
+        $URL = $FWLink + $LangCode
+        $Path = $Path + "\Server2016"
+    }
+    ElseIf ($OSVer -eq 'Server2019')
+    {
+        $FWlink = "https://go.microsoft.com/fwlink/p/?LinkID=2195167"
+        $URL = $FWLink + $LangCode
+        $Path = $Path + "\Server2019"
+    }
+    ElseIf ($OSVer -eq 'Server2022')
+    {
+        $FWLink = "https://go.microsoft.com/fwlink/p/?LinkID=2195280"
+        $URL = $FWLink + $LangCode
+        $Path = $Path + "\Server2022"
+    }
+    ElseIf ($OSVer -eq 'Server2025')
+    {
+        $FWLink = "https://go.microsoft.com/fwlink/p/?LinkID=2268694"
+        $URL = $FWLink + $LangCode
+        $Path = $Path + "\Server2025"
+    }
     Check-PathExists -Path $Path
     
-    # Download .cab file that contains products.xml, extract it
+    # Download file that contains products.xml (Win10/11) or actual ISO (Server)
     $ActualURL = Get-RedirectedUrl -URL $URL
-    $ProductsCab = Download-File -URL $ActualURL -Path $Path
-    Extract-File -File $ProductsCab -Path $Path
-    $XmlFile = Get-Item -Path "$Path\Extract\products.xml"
+    $TempDownload = Download-File -URL $ActualURL -Path $Path
 
-    # Check that $XmlFile actually exists
-    If (!(Test-Path -Path $XmlFile))
+    # Extract ESD for Win10/Win11
+    If ($OSVer -like "Win*")
     {
-        Return $Error[0]
-    }
-    Else
-    {
-        [xml]$Xml=Get-Content -Path $XmlFile
-    }
+        Extract-File -File $TempDownload -Path $Path
+        $XmlFile = Get-Item -Path "$Path\Extract\products.xml"
 
-    # Find the ESD link that matches the language and architecture
-    $Files = $Xml.MCT.Catalogs.Catalog.PublishedMedia.Files.File
-    ForEach ($File in $Files)
-    {
-        If (($File.FileName -like "*VOL*$($Lang).esd*") -and ($File.Architecture -like $Arch) -and ($File.Edition -notlike "*N"))
+        # Check that $XmlFile actually exists
+        If (!(Test-Path -Path $XmlFile))
         {
-            # Download actual ESD file
-            Download-File -URL $File.FilePath -Path $Path
-            $TempFile = $Path + "\" + $File.FileName
+            Return $Error[0]
+        }
+        Else
+        {
+            Write-Log "Parsing file $($XmlFile) to get correct ESD URL..."
+            [xml]$Xml=Get-Content -Path $XmlFile
+        }
+
+        # Find the ESD link that matches the language and architecture
+        $Files = $Xml.MCT.Catalogs.Catalog.PublishedMedia.Files.File
+        ForEach ($File in $Files)
+        {
+            If (($File.FileName -like "*VOL*$($Lang).esd*") -and ($File.Architecture -like $Arch) -and ($File.Edition -notlike "*N"))
+            {
+                # Download actual ESD file
+                Write-Log "Attempting download of $($File.FilePath) to $($Path)..."
+                Download-File -URL $File.FilePath -Path $Path
+                $Result = $Path + "\" + $File.FileName
+            }
         }
     }
-    Return $TempFile
+    # Just return downloaded ISO for Server
+    ElseIf ($OSVer -like "Server*")
+    {
+        $Result = $TempDownload
+    }
+    
+
+    Return $Result
 }
 
 
@@ -384,93 +471,103 @@ Function Create-LatestWindowsISO
 
     # Make sure we have a working directory
     Check-PathExists -Path $Path
-    
-    # Get latest $OSVer ESD file for language
-    $ESDFiles = Find-WindowsESD -OSVer $OSver -Arch $Arch -Lang $Lang -Path $Path
-    If ($ESDFiles.Count -gt 1)
+
+    # Get latest ESD or ISO file for language
+    $ESDFiles = Find-WindowsESDorISO -OSVer $OSver -Arch $Arch -Lang $Lang -Path $Path
+    #Handle duplicates - TODO figure out why Return sometimes gets Return'd 2x
+    $File = $ESDFiles[0]
+
+    # Extra ESD extract work if Win10/Win11 to get to an ISO
+    If ($OSVer -like "Win*")
     {
-        #Handle duplicates - TODO figure out why Return sometimes gets Return'd 2x
-        $File = $ESDFiles[0]
-    }
-    Else
-    {
-        $File = $ESDFiles
-    }
-    Write-Log -Message "Windows ESD file located:  $($File)"
-    $Images = Get-WindowsImage -ImagePath $File
+        Write-Log -Message "Windows ESD file located at $($File)"
+        $Images = Get-WindowsImage -ImagePath $File
 
-    # Create variables for making image and ISO
-    $TempFile = Get-Item $File
+        # Create variables for making image and ISO
+        $TempFile = Get-Item $File
 
-    $ImageVersion = $TempFile.BaseName.Substring(0, $TempFile.BaseName.LastIndexOf("."))
-    $ImageVersion = $OSVer + "-" + $ImageVersion
-    Write-Log -Message $ImageVersion
-    Write-Log -Message $ImageVersion.Length
+        $ImageVersion = $TempFile.BaseName.Substring(0, $TempFile.BaseName.LastIndexOf("."))
+        $ImageVersion = $OSVer + "-" + $ImageVersion
+        #Write-Log $ImageVersion
+        #Write-Log $ImageVersion.Length
 
-    $TempFileName = $TempFile.Name
-    $TempFileName = $TempFileName.Replace('.esd','.iso')
-    $ISO = $Path + "\" + $TempFileName
+        $TempFileName = $TempFile.Name
+        $TempFileName = $TempFileName.Replace('.esd','.iso')
+        $ISO = $Path + "\" + $TempFileName
 
-    $Mount = "$($env:TEMP)\Mount"
+        $Mount = "$($env:TEMP)\Mount"
 
-    $TempFile = $null
-    $TempFileName = $null
+        $TempFile = $null
+        $TempFileName = $null
 
-    # Find the ADK
-    If (Test-Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows Kits\Installed Roots")
-    {
-        $KitsRoot = Get-ItemPropertyValue -Path "HKLM:\Software\WOW6432Node\Microsoft\Windows Kits\Installed Roots" -Name KitsRoot10
-    }
-    Else
-    {
-        $KitsRoot = Get-ItemPropertyValue -Path "HKLM:\Software\Microsoft\Windows Kits\Installed Roots" -Name KitsRoot10
-    }
-
-    # Create a Mount folder to put extracted contents into
-    Check-PathExists -Path $Mount -DeleteIfExists $True
-
-    # Apply the first image (setup media) into Mount folder
-    Write-Log -Message "Expanding Setup Media image to $($Mount)..."
-    Expand-WindowsImage -ImagePath $File -Index 1 -ApplyPath $Mount
-    Write-Host ""
-
-    # Extract the third image (Windows PE + Setup) as boot.wim into the sources folder in Mount
-    Write-Log -Message "Exporting Windows PE image to $($Mount) as boot.wim..."
-    Export-WindowsImage -SourceImagePath $File -SourceIndex 3 -DestinationImagePath "$Mount\sources\boot.wim" -DestinationName "Microsoft Windows Setup ($Arch)" -Setbootable
-    Write-Host ""
-
-    # Find the images in the ESD and export to install.wim
-    ForEach ($Image in $Images)
-    {
-        If ((($Image.ImageName -like "*Pro*") -or ($Image.ImageName -like "*Enterprise*")) -and ($Image.ImageName -notlike "* N"))
+        # Find the ADK
+        If (Test-Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows Kits\Installed Roots")
         {
-            Try
-            {
-                $ImageIndex = $Image.ImageIndex
-                $ImageName = $Image.ImageName
+            $KitsRoot = Get-ItemPropertyValue -Path "HKLM:\Software\WOW6432Node\Microsoft\Windows Kits\Installed Roots" -Name KitsRoot10
+        }
+        Else
+        {
+            $KitsRoot = Get-ItemPropertyValue -Path "HKLM:\Software\Microsoft\Windows Kits\Installed Roots" -Name KitsRoot10
+        }
 
-                # Extract the desired OS image into the sources folder as install.wim
-                Write-Log -Message "Expanding selected $($ImageName) image to $($Mount)\sources as install.wim..."
-                Export-WindowsImage -SourceImagePath $File -SourceIndex $ImageIndex -DestinationImagePath "$Mount\sources\install.wim" -DestinationName $ImageName
-                Write-Host ""
-            }
-            Catch
+        # Create a Mount folder to put extracted contents into
+        Check-PathExists -Path $Mount -DeleteIfExists $True
+
+        # Apply the first image (setup media) into Mount folder
+        Write-Log -Message "Expanding Setup Media image to $($Mount)..."
+        Expand-WindowsImage -ImagePath $File -Index 1 -ApplyPath $Mount
+        Write-Host ""
+
+        # Extract the third image (Windows PE + Setup) as boot.wim into the sources folder in Mount
+        Write-Log -Message "Exporting Windows PE image to $($Mount) as boot.wim..."
+        Export-WindowsImage -SourceImagePath $File -SourceIndex 3 -DestinationImagePath "$Mount\sources\boot.wim" -DestinationName "Microsoft Windows Setup ($Arch)" -Setbootable
+        Write-Host ""
+
+        # Find the images in the ESD and export to install.wim
+        ForEach ($Image in $Images)
+        {
+            If ((($Image.ImageName -like "*Pro*") -or ($Image.ImageName -like "*Enterprise*")) -and ($Image.ImageName -notlike "* N"))
             {
-                Write-Log -Message "Error: $_"
+                Try
+                {
+                    $ImageIndex = $Image.ImageIndex
+                    $ImageName = $Image.ImageName
+
+                    # Extract the desired OS image into the sources folder as install.wim
+                    Write-Log -Message "Expanding selected $($ImageName) image to $($Mount)\sources as install.wim..."
+                    Export-WindowsImage -SourceImagePath $File -SourceIndex $ImageIndex -DestinationImagePath "$Mount\sources\install.wim" -DestinationName $ImageName
+                    Write-Host ""
+                }
+                Catch
+                {
+                    Write-Log -Message "Error: $_"
+                }
             }
         }
+
+        # Capture the ISO
+        Write-Log -Message "Creating $($ISO) from contents of $($Mount)..."
+        Push-Location $Mount
+        # This will throw an error, but will work - oscdimg does *not* seem to like being called from Powershell any other way though that I can find...
+        & "$($KitsRoot)Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg\oscdimg.exe" "-l$ImageVersion"  '-o' '-u2' '-m' '-udfver102' "-bootdata:1#pEF,e,bEFI\Microsoft\boot\efisys.bin" "$Mount" "$ISO" | Out-Host
+        Pop-Location
+        Write-Host ""
+
+        # Clean up the temporary folder
+        Check-PathExists -Path $Mount -DeleteIfExists $True
+
+        Write-Log "ISO located at $($ISO)"
+        Write-Host ""
+        Write-Host ""
+        Write-Host ""
     }
-
-    # Capture the ISO
-    Write-Log -Message "Creating $($ISO) from contents of $($Mount)..."
-    Push-Location $Mount
-    # This will throw an error, but will work - oscdimg does *not* seem to like being called from Powershell any other way though that I can find...
-    & "$($KitsRoot)Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg\oscdimg.exe" "-l$ImageVersion"  '-o' '-u2' '-m' '-udfver102' "-bootdata:1#pEF,e,bEFI\Microsoft\boot\efisys.bin" "$Mount" "$ISO" | Out-Host
-    Pop-Location
-    Write-Host ""
-
-    # Clean up the temporary folder
-    Check-PathExists -Path $Mount -DeleteIfExists $True
+    ElseIf ($OSVer -like "Server*")
+    {
+        Write-Log "ISO located at $($File)"
+        Write-Host ""
+        Write-Host ""
+        Write-Host ""
+    }
 }
 
 
@@ -479,10 +576,10 @@ Function Create-LatestWindowsISO
 # MAIN #
 ########
 Write-Log -Message "Script start:    $($Script_Start_Time)"
-Write-Host ""
+Write-Log " "
 
 
-$ISO = Create-LatestWindowsISO -Path $Path -OSVer $OSVer -Arch $Arch -Lang $Lang
+Create-LatestWindowsISO -Path $Path -OSVer $OSVer -Arch $Arch -Lang $Lang
 
 
 # How long did the whole thing take?
